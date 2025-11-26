8e6667affabd6f5947c841bd7822ff6279e58f31
e-hartney <54752586+e-hartney@users.noreply.github.com>
2025-11-26 02:09:10 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+# app.py
+
+import streamlit as st
+from location_logic import assess_location_request
+
+st.title("Location Agreement Decision Tool")
+
+st.subheader("Location type")
+key_location = st.checkbox("Key location (i.e. must be that specific location)")
+location_outside_main_filming_country = st.checkbox("Location is outside of main filming country")
+
+owner_type = st.selectbox(
+    "Owner type",
+    ["private individual", "private company", "public authority", "charity", "other"],
+)
+
+st.subheader("Location characteristics")
+public_access = st.checkbox("Location will be open to public during filming")
+residential_area = st.checkbox("Location is in or near a residential area")
+visible_artwork_or_brands = st.checkbox("Artwork or branding visible during filming")
+unusual_personal_data_risk = st.checkbox("Risk of exposure to personal data (e.g. location is a working hospital)")
+unaccompanied_minors_present = st.checkbox("Unaccompanied minors likely to be present (e.g. location is a working school)")
+limited_parking = st.checkbox("Limited parking facilities")
+
+st.subheader("Filming requirements")
+non_routine_structural_changes = st.checkbox("Non-routine structural changes required")
+pyrotechnics = st.checkbox("Pyrotechnics to be used")
+drones = st.checkbox("Drones to be used")
+animals = st.checkbox("Animals to be used")
+extreme_stunts = st.checkbox("Extreme stunts to be carried out")
+crowd_scene = st.checkbox("Crowd scene required")
+
+
+if st.button("Assess"):
+    result = assess_location_request(
+    key_location = key_location,
+    location_outside_main_filming_country = location_outside_main_filming_country,
+    owner_type = owner_type,
+    public_access = public_access,
+    residential_area =  residential_area,
+    visible_artwork_or_brands = visible_artwork_or_brands,
+    unusual_personal_data_risk = unusual_personal_data_risk,
+    unaccompanied_minors_present = unaccompanied_minors_present,
+    non_routine_structural_changes = non_routine_structural_changes,
+    pyrotechnics = pyrotechnics,
+    drones = drones,
+    animals = animals,
+    extreme_stunts = extreme_stunts,
+    crowd_scene = crowd_scene,
+    limited_parking = limited_parking,
+)
+
+    if result.inform_legal == True:
+        st.subheader("Contact legal for:")
+        for r in result.legal_reasons:
+           st.write(f"- {r}")
+
+    if len(result.clearance_tasks) != 0 or len(result.production_tasks) != 0:
+        st.subheader("Inform the following other teams:")
+
+        if len(result.clearance_tasks) != 0:
+            st.write("**Clearances:**")
+            for r in result.clearance_tasks:
+                st.write(f"- {r}")
+
+        if len(result.production_tasks) != 0:
+            st.write("**Production:**")
+            for r in result.production_tasks:
+                st.write(f"- {r}")
+
+    if len(result.contract_riders) != 0:
+        st.subheader("Include the following contract riders:")
+        for r in result.contract_riders:
+           st.write(f"- {r}")
+    
+    if len(result.legal_reasons) == 0 and len(result.clearance_tasks) == 0 and len(result.production_tasks) ==0 and len(result.contract_riders) ==0:
+        st.write("Proceed with template agreement - contact legal with any queries")
+
---
+++ location_logic.py
@@ -0,0 +1,106 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class LocationAssessmentResult:
+    inform_legal: bool
+    legal_reasons: List[str]  
+    clearance_tasks: List[str]
+    production_tasks: List[str]
+    contract_riders: List[str]
+
+
+def assess_location_request(
+    key_location: bool,
+    location_outside_main_filming_country: bool,
+    owner_type: str,
+    public_access: bool,
+    residential_area: bool,
+    visible_artwork_or_brands: bool,
+    unusual_personal_data_risk: bool,
+    unaccompanied_minors_present: bool,
+    non_routine_structural_changes: bool,
+    pyrotechnics: bool,
+    drones: bool,
+    animals: bool,
+    extreme_stunts: bool,
+    crowd_scene: bool,
+    limited_parking: bool,
+) -> LocationAssessmentResult:
+    inform_legal = False
+    legal_reasons = []
+    clearance_tasks = []
+    production_tasks = []
+    contract_riders = []
+
+
+
+    # Inform legal team
+    if key_location:
+        inform_legal = True
+        legal_reasons.append("key location")  
+        # legal will need to allocate resources
+   
+    if location_outside_main_filming_country:
+        inform_legal = True
+        legal_reasons.append("location outside main filming country") 
+        
+    if owner_type == "charity" or owner_type == "other":
+        inform_legal = True
+        legal_reasons.append("owner is charity or non-standard entity")
+
+    if unusual_personal_data_risk:
+        inform_legal = True
+        legal_reasons.append("unusual personal data risk")  
+        # Legal may need to provide further policy framework and/or privacy notices/consents
+
+    if unaccompanied_minors_present:
+        inform_legal = True
+        legal_reasons.append("unaccompanied minors likely to be present")  
+        # Legal may need to provide further policy framework and/or parental consent forms
+    
+    if non_routine_structural_changes:
+        inform_legal = True
+        legal_reasons.append("non-routine structural changes required")
+
+    # Inform other teams
+    if visible_artwork_or_brands:
+        clearance_tasks.append("artworks / brands visible at location")
+
+    if public_access:
+        production_tasks.append("public access / notice to public required")
+
+    if residential_area:
+        production_tasks.append("residential area / notices to residents required")
+
+    if limited_parking:
+        production_tasks.append("limited parking / plan required")
+
+    if drones:
+        production_tasks.append("drone use / check logistics and permissions")
+        # drones included in standard contract template
+
+    if animals:
+        production_tasks.append("animal use / plan logistics on property")
+        # animals included in standard contract template
+
+
+    # Riders to include in contract
+    if pyrotechnics: 
+        contract_riders.append("pyrotechnics rider")
+
+    if extreme_stunts: 
+        contract_riders.append("extreme stunts rider")
+
+    if crowd_scene: 
+        contract_riders.append("crowd rider")
+    
+
+    return LocationAssessmentResult(
+        inform_legal = inform_legal,
+        legal_reasons = legal_reasons,
+        clearance_tasks = clearance_tasks,
+        production_tasks = production_tasks,
+        contract_riders = contract_riders,
+    )   
